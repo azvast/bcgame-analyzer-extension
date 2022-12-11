21e1f7ba36015ff819e3bb100d06a57a77e9ef9f
azvast <azvast38@gmail.com>
2022-12-11 13:26:15 +0900

    update

--- inject.js
+++ inject.js
@@ -1,32 +1,33 @@
-function openWindow() {
-    // Click history button
-    document.querySelector("#root > div.s1o3yrjt.page-max-width-wrap.small-screen > div.game-data > div.left-data > div.latest > div > div.tabs-scroll > div > button:nth-child(2)").click();
-    
-    var newWindow = window.open('https://bcgraph.netlify.app','','width=400,height=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,scrollbars=no,fullscreen=no,resizable=no,location=no,directories=no,status=yes');  
-    
-    var prevHash = '';
-    setInterval(() => {
-        var latestHash = document.querySelector("#root > div.s1o3yrjt.page-max-width-wrap.small-screen > div.game-data > div.left-data > div.latest > div > div.tabs-view > div > div.h4j5hip > table > tbody > tr:nth-child(1)");
-
-        if (latestHash != prevHash) {
-            prevHash = latestHash;
-            newWindow.postMessage({ hash: latestHash }, '*');
-        }
-    }, 2000);
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function init() {
-    const graphButton = document.createElement('button');
-    graphButton.appendChild(document.createTextNode('Analysis Graph'));
+async function openWindow() {
+	var btnHistory = null;
 
-    graphButton.onclick = function() {
-        openWindow();
-    }
+	do {
+		await delay(2000);
 
-    const box = document.querySelector("#root > div.s1o3yrjt.page-max-width-wrap.small-screen > div.t1tuhu22");
-    box.appendChild(graphButton);
+		btnHistory = document.querySelector("#root > div.s1o3yrjt.page-max-width-wrap > div.game-data > div.left-data > div.latest > div > div.tabs-scroll > div > button:nth-child(2)")
+	} while (!btnHistory);
+
+	btnHistory.click();
+	
+	var newWindow = await window.open('https://bcgraph.netlify.app','','fullscreen=yes');
+	if (!newWindow) {
+		window.location.reload();
+	} else {
+		var prevHash = '';
+		setInterval(() => {
+			var latestHash = document.querySelector("#root > div.s1o3yrjt.page-max-width-wrap > div.game-data > div.left-data > div.latest > div > div.tabs-view > div > div.h4j5hip > table > tbody > tr:nth-child(1) > td:nth-child(3) > div > input[type=text]").value;
+				if (latestHash != prevHash) {
+						prevHash = latestHash;
+						newWindow.postMessage({ hash: latestHash }, '*');
+				}
+		}, 2000);
+	}
 }
 
 setTimeout(() => {
-    init();
-}, 3000);
+	openWindow();
+}, 2000);
